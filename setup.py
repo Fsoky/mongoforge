cc339a943f1d3480aa4ddc45e7b8e4e14c351ea5
Fsoky <cyberuest0x12@gmail.com>
2026-03-26 02:49:27 +0500

    Add setup.py and setup.cfg for package configuration

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from io import open
+
+
+def read(filename: str) -> str:
+    with open(filename, "r", encoding="utf-8") as file:
+        return file.read()
+
+
+setup(
+    name="mongoforge",
+    version="1.0.0",
+    description="Magic filters for MongoDB",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    author="Fsoky",
+    author_email="cyberuest0x12@gmail.com",
+    url="https://github.com/Fsoky/mongoforge",
+    keywords="mongodb pymongo utils",
+    packages=find_packages()
+)
